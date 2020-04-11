4a8c663765a8e6ce1d8ccddf6b7797dfa6d81f84
Muktipriyo <oisam060@gmail.com>
2020-04-11 19:25:57 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace LatihanInheritanceReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mukti", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Thomas", 35, 190302, "Guru Matematika");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Ayana", 21, 10506, "Ayanajkt48@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+    
